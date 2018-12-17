3f4e2bbe848e4d6d8e53ee956d21ebbe984d8105
rameshkkumar7 <ramesh.krishna@promonsoft.com>
2018-12-17 18:20:01 +0530

    Dashboard title text changed

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,7 +6,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Dashboard1
+        Dashboard
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
