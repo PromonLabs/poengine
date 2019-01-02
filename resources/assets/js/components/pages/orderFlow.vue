87e155843c05b3956e479d2e0949cecb40209411
Christudasan <christudasan.a@promonsoft.com>
2019-01-02 17:56:12 +0530

    List the order flow

--- resources/assets/js/components/pages/orderFlow.vue
+++ resources/assets/js/components/pages/orderFlow.vue
@@ -1,6 +1,37 @@
 <template>
 <div class="col-xs-12 col-md-12" style="margin-top:20px;">
-    sajin
+    <h3>Order Flow for {{ this.OfferDetailsId }}<a id="goBack" style="float:right; cursor:pointer; font-size:14px;color:#98BCDE;"><i class="fa fa-arrow-left" aria-hidden="true"></i> Return to list</a></h3>
+    <table class="table table-striped table-hover">
+        <thead class="thead-blue">
+            <tr>
+                <th>Sequence</th>
+                <th>Action</th>
+                <th>Status</th>
+                <th>Options</th>
+            </tr>
+        </thead>
+        <tbody v-for="orderFlow in orderFlowDataList" :key='orderFlow.step'>
+            <div id="circle"></div>
+            <tr>
+                <td>{{ orderFlow.step }}</td>
+                <td>{{ orderFlow.action.name }}</td>
+                <td>{{ orderFlow.action_status.name }}</td>
+                <td>
+                    <div v-if="orderFlow.action_status.name != 'completed' && orderFlow.action_status.name != 'skipped'">
+                         <button type="button" class="skip-order-step-btn small-button">Skip</button>
+                    </div>
+                </td>
+            </tr>
+            <hr id="circle-line"></hr>
+                    <div id="action-step" style="border:3px;" data-toggle="tooltip">
+                        <div style=" color:#fff; padding:5px 0;">{{orderFlow.action_status.name}}</div>
+                        <div id="action-step-div">
+                            <span style="padding:5px 10px; margin:5px 0px; font-weight:bold;">
+                                STEP </span><br>{{orderFlow.action.name}}
+                        </div>
+                    </div>
+        </tbody>
+    </table>
 </div>
 </template>
 <style>
@@ -44,13 +75,16 @@
        data () {
     return {
       search: '',
-      OfferDetailId : null,
+      OfferDetailsId : null,
+      orderFlowDataList : null,
 
     }
   },
     mounted() {
         this.$root.$on('viewOrderFlowData', orderFlowData => {
           console.log(orderFlowData);
+          this.OfferDetailsId = orderFlowData.offerDetails.id;
+          this.orderFlowDataList = orderFlowData.orderActionDetails;
         })
     },
     methods: {
